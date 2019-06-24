ef83ebc7b3f9bcc08bb1a9fc04c45d7b2cf1542d
Alex Siegberg and Josh Mabry <josh@>
2019-06-24 16:31:05 -0700

    add logic for creating card array and randomizing it

--- src/ping-pong.js
+++ src/ping-pong.js
@@ -1,16 +1,21 @@
-export function pingPong(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push('ping-pong');
-    } else if (i % 3 === 0) {
-      output.push('ping');
-    } else if (i % 5 === 0) {
-      output.push('pong');
-    } else {
-      output.push(i);
-    }
-  }
-
-  return output;
+function Card(name, image) {
+  this.name = name;
+  this.image = image;
 }
+
+let queenCard = new Card('Queen', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Poker-sm-213-Qs.png/170px-Poker-sm-213-Qs.png');
+
+let kingCard = new Card('King', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Poker-sm-222-Kh.png');
+
+let jackCard = new Card('Jack', 'https://previews.123rf.com/images/mannaggia/mannaggia0911/mannaggia091100007/5957160-jack-of-spades-playing-card.jpg');
+
+export let cardArray = [queenCard, kingCard, jackCard, queenCard, kingCard, jackCard];
+
+/* eslint-disable */
+export function cardJumble(cardArray) {
+  cardArray.sort(function (a, b) {return 0.5 - Math.random();
+  });
+
+  return cardArray;
+}
+/* eslint-enable */
